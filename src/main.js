348c6c1eba24eea49ab86763b1bcd07661cd00a5
nnamm <t.nnamm@gmail.com>
2020-03-22 19:18:54 +0900

    OGPにFB追加

--- src/main.js
+++ src/main.js
@@ -39,6 +39,16 @@ export default function (Vue, { router, head, isClient, appOptions }) {
     property: 'og:description',
     content: '空気感フォトグラファーのポートフォリオサイトです。Gridsomeで作りました。写真、グラフィック、ブログなどの作品を掲載しています。'
   })
+  // OGP(Facebook)
+  head.meta.push({
+    property: 'article:publisher',
+    content: 'https://www.facebook.com/t.hanamura.photography/'
+  })
+  head.meta.push({
+    property: 'fb:app_id',
+    content: '652239472256159'
+  })
+  // OGP(Twitter)
   head.meta.push({
     name: 'twitter:card',
     content: 'summary_large_image'
